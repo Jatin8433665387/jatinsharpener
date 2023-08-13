3116e2f49234ee4281203caae887db53571f9a7c
Jatin8433665387 <91005701+Jatin8433665387@users.noreply.github.com>
2023-08-13 14:23:07 +0530

    ass 3

---
+++ dom.js
@@ -0,0 +1,8 @@
+//console.dir(document)
+//.log(document.defaultView);
+//console.log(document.all[10]);
+//document.all[10].textContent = "hello"
+var a = document.getElementById('header-title');
+console.log(a);
+a.innerText = "god";
+a.innerHTML = '<h3>"hello"</h3>';
