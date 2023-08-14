5fb8df78c5ffd504932577c5676381f9fcddb930
Jatin8433665387 <91005701+Jatin8433665387@users.noreply.github.com>
2023-08-15 01:31:48 +0530

    Add files via upload

---
+++ dom2.js
@@ -0,0 +1,60 @@
+var items = document.querySelector("#items");
+
+// parentNode
+//console.log(items.parentNode);
+//items.parentNode.style.backgroundColor = "grey"
+//console.log(items.parentNode.parentNode)
+
+//parentElement
+console.log(items.parentElement);
+// items.parentElement.style.backgroundColor = "blue";
+// console.log(items.parentElement.parentElement);
+
+//childNode
+//console.log(items.childNodes);
+
+//childrens
+//console.log(items.children);
+//console.log(items.children[-1]);
+
+//firstchild
+//console.log(items.firstChild)
+
+//firstelementchild(items)
+//console.log(items.firstElementChild)
+
+//firstchild
+//console.log(items.lastChild)
+
+//firstelementchild(items)
+//console.log(items.lastElementChild)
+
+// nextsibling
+//console.log(items.nextSibling);
+
+// nextsiblingelement
+//console.log(items.nextElementSibling);
+
+//previous sibling
+//console.log(items.previousSibling);
+
+// previous elememt siblimg
+//console.log(items.previousElementSibling)
+
+//create new element
+//create new div
+//var newdiv = document.createElement('div');
+
+//create class
+//newdiv.className = "hell"
+
+//create id
+//newdiv.id = "hh"
+
+// new attr
+//newdiv.setAttribute('title','jat');
+
+// create text Node
+//var newd = document.createTextNode("hell")
+//newdiv.appendChild(newd)
+//console.log(newdiv)
