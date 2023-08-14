5ed0baa9c162e996b660fecb7f2c1d6fe6ad9379
Jatin8433665387 <91005701+Jatin8433665387@users.noreply.github.com>
2023-08-14 22:20:09 +0530

    query selector and queryselector all

--- dom.js
+++ dom.js
@@ -19,13 +19,33 @@
 //}
 
 // get elements by tagname
-var li = document.getElementsByTagName("li");
-console.log(li);
-console.log(li[1]);
-li[1].textContent = "hello 2";
-li[1].style.fontWeight = "bold";
-li[1].style.backgroundColor = "yellow";
-// to manipulate all elements in list
-for(var i=0;i < li.length; i++){
-	li[i].style.backgroundColor = "orange";
-}
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "hello 2";
+// li[1].style.fontWeight = "bold";
+// li[1].style.backgroundColor = "yellow";
+// // to manipulate all elements in list
+// for(var i=0;i < li.length; i++){
+// 	li[i].style.backgroundColor = "orange";
+// }
+
+// query selector
+
+var input = document.querySelector("input");
+input.value = "hello bhailog";
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "send";
+
+var first = document.querySelector('.list-group-item');
+first.style.backgroundColor = "green";
+
+var last = document.querySelector('.list-group-item:last-child');
+last.style.color = "red";
+
+var middle = document.querySelector('.list-group-item:nth-child(2)');
+middle.style.backgroundColor = "green";
+
+var middlee = document.querySelector('.list-group-item:nth-child(3)');
+middlee.style.color = "white";
